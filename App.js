41f493cf3d0dff2d331abbfabfea69b5b55e0b11
Hoyeon Lee <vnzk0301@naver.com>
2022-10-25 14:35:17 +0900

    delete fnc added

--- App.js
+++ App.js
@@ -7,25 +7,61 @@ import {
   KeyboardAvoidingView,
   TextInput,
   TouchableOpacity,
+  ScrollView,
+  FlatList,
+  Alert,
 } from "react-native";
 import Task from "./components/Task";
+import react, { useState } from "react";
 
 export default function App() {
+  const [todoList, setTodoList] = useState(["rqfqreof", "irif"]);
+  const [input, setInput] = useState("");
+
+  function handleDelete(index) {
+    let copy = [...todoList];
+    copy.splice(index, 1);
+    setTodoList(copy);
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
-        <View style={styles.items}>
-          <Task task="와우우ㅜ" />
-          <Task task="rㄹㅈㄹㅈrfrfwfrrwrfrfffㅈㄹㅈrfrfwfrrwrfrㅈㄹㅈrfrfwfrrwrfrffㄹ" />
-        </View>
+        <ScrollView style={styles.items}>
+          {todoList.map((todo, i) => (
+            <TouchableOpacity
+              key={i}
+              onPress={() => {
+                handleDelete(i);
+              }}
+            >
+              <Task task={todo} />
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
       </View>
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.writeTaskWrapper}
       >
-        <TextInput style={styles.input} placeholder="write tasks here" />
-        <TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder="write tasks here"
+          value={input}
+          onChangeText={(e) => {
+            setInput(e);
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            if (!todoList.includes(input)) {
+              setTodoList([...todoList, input]);
+              setInput("");
+            } else {
+              Alert.alert("already there!");
+            }
+          }}
+        >
           <View style={styles.addWrapper}>
             <Text style={styles.addIcon}>+</Text>
           </View>
@@ -49,7 +85,8 @@ const styles = StyleSheet.create({
   items: {},
   writeTaskWrapper: {
     position: "absolute",
-    bottom: 50,
+    height: 100,
+    bottom: 10,
     marginHorizontal: 20,
     // borderWidth: 3,
     flexDirection: "row",
